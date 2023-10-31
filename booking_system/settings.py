65344a606d7a45370f4e12ba64fffa09256b2509
hcdiekmann <hcdiekmann@hotmail.com>
2023-11-01 10:54:09 +1300

    update requirements

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_value')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')  # default value is True
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-
 
 # Application definition
 
